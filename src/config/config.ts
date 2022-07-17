84aa3a918967a14184e79c41f5863896643154ab
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-17 17:09:01 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,19 +14,6 @@ export const configVars = {
     },
     blockExplorerUrl: "https://cronoscan.com/",
   },
-    rpcNetwork: {
-    rpcUrl: "https://evm-t3.cronos.org/",
-    chainId: 338,
-    chainIdHex: "0x152",
-    chainName: "Cronos Testnet",
-    chainType: "testnet",
-    nativeCurrency: {
-      name: "CRO",
-      symbol: "CRO",
-      decimals: 18,
-    },
-    blockExplorerUrl: "https://cronos.crypto.org/explorer/testnet3/",
-  },
   erc20: {
     address: "0x47ef2d14386d6d99637eec6a3b1c30f41c160cc3", // on Cronos Testnet
     address_mainnet: "0xecee7183cb41f382972d436862d690a71520318a", // on Cronos Mainnet
