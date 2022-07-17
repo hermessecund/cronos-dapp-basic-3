c58c382efd69458911cd47e9a678b87617b59de4
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-17 17:02:25 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,6 @@
 // config/config.ts
 export const configVars = {
   mode: "regular",
-  rpcNetwork: {
-    rpcUrl: "https://evm-t3.cronos.org/",
-    chainId: 338,
-    chainIdHex: "0x152",
-    chainName: "Cronos Testnet",
-    chainType: "testnet",
-    nativeCurrency: {
-      name: "CRO",
-      symbol: "CRO",
-      decimals: 18,
-    },
-    blockExplorerUrl: "https://cronos.crypto.org/explorer/testnet3/",
-  },
   rpcNetwork_mainnet: {
     rpcUrl: "https://evm.cronos.org/",
     chainId: 25,
@@ -26,6 +13,19 @@ export const configVars = {
       decimals: 18,
     },
     blockExplorerUrl: "https://cronoscan.com/",
+  },
+    rpcNetwork: {
+    rpcUrl: "https://evm-t3.cronos.org/",
+    chainId: 338,
+    chainIdHex: "0x152",
+    chainName: "Cronos Testnet",
+    chainType: "testnet",
+    nativeCurrency: {
+      name: "CRO",
+      symbol: "CRO",
+      decimals: 18,
+    },
+    blockExplorerUrl: "https://cronos.crypto.org/explorer/testnet3/",
   },
   erc20: {
     address: "0x47ef2d14386d6d99637eec6a3b1c30f41c160cc3", // on Cronos Testnet
