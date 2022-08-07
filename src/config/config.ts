5ccc54b503ce8c2c54f97490ed23f915c19bf74a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-08-07 16:44:11 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,19 +2,19 @@
 export const configVars = {
  
   rpcNetwork: {
-    mode: "regular",
-    rpcUrl: "https://evm-t3.cronos.org/",
-    chainId: 338,
-    chainIdHex: "0x152",
-    chainName: "Cronos Testnet",
-    chainType: "testnet",
-    nativeCurrency: {
-      name: "CRO",
-      symbol: "CRO",
-      decimals: 18,
-    },
-    blockExplorerUrl: "https://cronos.crypto.org/explorer/testnet3/",
-  },
+ //   mode: "regular",
+ //   rpcUrl: "https://evm-t3.cronos.org/",
+ //   chainId: 338,
+ //   chainIdHex: "0x152",
+ //   chainName: "Cronos Testnet",
+  //  chainType: "testnet",
+ //   nativeCurrency: {
+  //    name: "CRO",
+//      symbol: "CRO",
+     // decimals: 18,
+   // },
+  //  blockExplorerUrl: "https://cronos.crypto.org/explorer/testnet3/",
+ // },
   rpcNetwork_mainnet: {
     
     rpcUrl: "https://evm.cronos.org/",
@@ -30,7 +30,7 @@ export const configVars = {
     blockExplorerUrl: "https://cronoscan.com/",
   },
   erc20: {
-    address: "0x47ef2d14386d6d99637eec6a3b1c30f41c160cc3", // on Cronos Testnet
+   // address: "0x47ef2d14386d6d99637eec6a3b1c30f41c160cc3", // on Cronos Testnet
     address_mainnet: "0xecee7183cb41f382972d436862d690a71520318a", // on Cronos Mainnet
   },
   logo: {
